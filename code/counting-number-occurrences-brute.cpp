917f299073223ef81f1a735c6cef75d271f7d20d
Vignesh Natarajan <vignesh.natarajan92@gmail.com>
2014-05-09 00:59:30 +0530

    counting number occurrences - brute force solution

---
+++ code/counting-number-occurrences-brute.cpp
@@ -0,0 +1,57 @@
+//Function to convert integers to string
+
+
+string intToString(int n) {
+
+    stringstream ss;
+
+    ss << n;
+
+    
+
+    return ss.str();
+
+}
+
+
+int main()
+
+{
+
+string s
+
+for(int i=1;i<=N;i++)
+
+     s+=intToString(i);
+
+
+string digit="0123456789";
+
+
+vector<int> count;
+
+
+for(int i=0;i<dig.length();i++)
+
+{
+
+     int cnt=0;
+
+     for(int j=0;j<s.length();j++)
+
+          if(s[j]==digit[i])cnt++;
+
+
+     count.push_back(cnt);
+
+}
+
+
+for(int i=0;i<cnt.size();i++)
+
+     cout<<"Number of occurrences of "<<i<<" :"<<count[i]<<endl;
+
+
+return 0;
+
+}
